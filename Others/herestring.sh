14a31537b4c6f1c1d381fe27f2d40ebbbad92a2a
lucascosm3 <lcds1944@gmail.com>
2024-05-09 08:15:48 -0300

    feat: enable/disabling debug in the script

--- Others/herestring.sh
+++ Others/herestring.sh
@@ -2,5 +2,7 @@
 
 IFS=:
 while read -a user; do
+  set -xv # enabling debug in the script
   echo "the user ${user[0]} user the shell ${user[6]}"
+  set -xv # disabling debug in the script
 done <<< "lucascosme:x:1000:1000:,,,:/home/lucascosme:/usr/bin/zsh"
