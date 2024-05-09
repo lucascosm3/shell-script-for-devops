df8edce653c5cd58b8d7372990f1d3944040513d
lucascosm3 <lcds1944@gmail.com>
2024-05-09 06:36:11 -0300

    feat:working with Variable Indirection

--- Native Functions/expansions.sh
+++ Native Functions/expansions.sh
@@ -1,30 +1,40 @@
 #!/usr/bin/env bash
 
-NAME=${1:-lucas} # If the parameter is not passed, it will use the declared default
-NOME=${1:+lucas}
-NOME=$1
+#NAME=${1:-lucas} # If the parameter is not passed, it will use the declared default
+#NOME=${1:+lucas}
+#NOME=$1
+#
+#NOME_2=${NOME:=lucas}
+#
+#echo "NOME: $NOME"
+#echo "NOME_2: $NOME_2"
+#
+#NOME=${1:?"Null variable"} # If you do not define a default value, but rather the user, if this is not met, null will be given
+#
+#echo $NOME
+#
+## Array
+#
+#fruits=(
+#  'Pineapple'
+#  'Orange'
+#  'Banana'
+#)
+#
+#echo ${fruits[@]} # returning the items within the array
+#echo ${#fruits[@]} # returning the number of items within the array
+#echo ${!fruits[@]} # returning the position of items within the array
+#
+#for i in ${!fruits[@]}; do
+#  echo ${fruits[$i]}
+#done
+#
+## Variable Indirection
 
-NOME_2=${NOME:=lucas}
+NAME="lucas"
+NAME_2="cosme"
+NAME_3="silva"
 
-echo "NOME: $NOME"
-echo "NOME_2: $NOME_2"
-
-NOME=${1:?"Null variable"} # If you do not define a default value, but rather the user, if this is not met, null will be given
-
-echo $NOME
-
-# Array
-
-fruits=(
-  'Pineapple'
-  'Orange'
-  'Banana'
-)
-
-echo ${fruits[@]} # returning the items within the array
-echo ${#fruits[@]} # returning the number of items within the array
-echo ${!fruits[@]} # returning the position of items within the array
-
-for i in ${!fruits[@]}; do
-  echo ${fruits[$i]}
+for variable_name in ${!NAME*}; do
+  echo "Variable Name: ${!variable_name}" # returns the value of variables in list
 done
